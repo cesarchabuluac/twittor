ac7438d76c32143e6e6b1bbaeb22dc033dc582f1
Cesar chab <cesarchabuluac@gmail.com>
2020-12-22 10:24:13 -0600

    Change version sw

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('js/libs/sw-utils.js');
 
 
 //creando el nombre de cache a usar
-const STATIC_CACHE      = 'static-v2';
+const STATIC_CACHE      = 'static-v3';
 const DYNAMIC_CACHE     = 'dynamic-v1';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
